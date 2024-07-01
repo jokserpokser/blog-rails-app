3d7ebec3b20da87529ee022b581048957dcaf3c4
Eljan Peralta <eljan233@gmail.com>
2024-07-01 21:02:07 +0800

    Added Model Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_29_031431) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_01_125802) do
   create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "body"
     t.datetime "created_at", null: false
